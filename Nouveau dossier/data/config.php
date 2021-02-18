4d22c2188772165d07acc9f52ceab45ba90d7bd4
JonathanRixhon <58252485+JonathanRixhon@users.noreply.github.com>
2021-02-18 18:05:01 +0100

    correction

---
+++ Nouveau dossier/data/config.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    'allemagne' =>
+    [
+        'capital-name' => 'berlin',
+        'flag-file' => 'germany-flag-icon-256.png'
+    ],
+    'belgique' =>
+    [
+        'capital-name' => 'bruxelles',
+        'flag-file' => 'belgium-flag-icon-256.png'
+    ],
+    'corée du nord' =>
+    [
+        'capital-name' => 'pyongyang',
+        'flag-file' => 'north-korea-flag-icon-256.png'
+    ],
+    'afrique du sud' =>
+    [
+        'capital-name' => 'johannesbourg',
+        'flag-file' => 'south-africa-flag-icon-256.png'
+    ],
+];
